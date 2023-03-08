48ed82cf9f66e0f81e899e91b71aff3d84c0b724
xu.pengfei <1508075252@qq.com>
2023-03-08 14:01:25 +0800

    feat: tenant-租户日志

--- src/main/java/com/faber/config/filter/RequestAgainFilter.java
+++ src/main/java/com/faber/config/filter/RequestAgainFilter.java
@@ -30,6 +30,7 @@ import java.util.List;
  * 2. 包装response，可以读取返回内容；
  * 3. 记录请求日志；
  * 4. 销毁上下文中记录的登录用户信息；
+ *
  * @author xu.pengfei
  * @date 2022/11/28 11:39
  */
@@ -66,63 +67,66 @@ public class RequestAgainFilter implements Filter {
         outputStream.flush();
         outputStream.close();
 
-        if (!NO_LOG_APIS.contains(requestWrapper.getRequestURI())) {
-            String logNoRet = responseWrapper.getHeader("LogNoRet");
-            LogApi logApi = new LogApi();
-
-            logApi.setBiz(StrUtil.toString(requestWrapper.getAttribute("FaLogBiz")));
-            logApi.setOpr(StrUtil.toString(requestWrapper.getAttribute("FaLogOpr")));
-            logApi.setCrud((LogCrudEnum) requestWrapper.getAttribute("FaLogCrud"));
-
-            // request basic information
-            logApi.setUrl(requestWrapper.getRequestURI());
-            logApi.setMethod(requestWrapper.getMethod());
-            logApi.setAgent(requestWrapper.getHeader("User-Agent"));
-
-            // 解析agent字符串
-            UserAgent ua = UserAgentUtil.parse(logApi.getAgent());
-            logApi.setOs(ua.getOs().toString());
-            logApi.setBrowser(ua.getBrowser().toString());
-            logApi.setVersion(ua.getVersion());
-            logApi.setMobile(ua.isMobile() ? true : false);
-
-            // 自定义Header信息
-            logApi.setFaFrom(requestWrapper.getHeader(CommonConstants.FA_FROM));
-            try {
-                if (StrUtil.isNotEmpty(requestWrapper.getHeader(CommonConstants.FA_VERSION_CODE))) {
-                    logApi.setVersionCode(Long.parseLong(requestWrapper.getHeader(CommonConstants.FA_VERSION_CODE)));
-                }
-            } catch (Exception e) {
-                log.error(e.getMessage(), e);
-            }
-            logApi.setVersionName(requestWrapper.getHeader(CommonConstants.FA_VERSION_NAME));
-
-            logApi.setCrtHost(IpUtils.getRequestIp(requestWrapper));
-            logApi.setRequest(requestWrapper.getBody());
-            logApi.setReqSize(logApi.getRequest().length());
-
-            logApi.setRetStatus(responseWrapper.getStatus());
-
-            if ("1".equals(logNoRet) && !"200".equals(logApi.getRetStatus())) {
-                logApi.setResponse("");
-            } else {
-                logApi.setResponse(responseData);
-            }
-
-            logApi.setRetSize(responseData.length());
-
-            logApi.setDuration(System.currentTimeMillis() - startTime);
-
-            // 获取IP地址
-            IpAddr ipAddr = IpUtils.getIpAddrByApi(logApi.getCrtHost());
-            if (ipAddr != null) {
-                logApi.setPro(ipAddr.getPro());
-                logApi.setCity(ipAddr.getCity());
-                logApi.setAddr(ipAddr.getAddr());
-            }
-
-            logApiBiz.save(logApi);
+        if (NO_LOG_APIS.contains(requestWrapper.getRequestURI())) {
+            BaseContextHandler.remove(); // 销毁上下文中记录的登录用户信息
+            return;
         }
+
+        String logNoRet = responseWrapper.getHeader("LogNoRet");
+        LogApi logApi = new LogApi();
+
+        logApi.setBiz(StrUtil.toString(requestWrapper.getAttribute("FaLogBiz")));
+        logApi.setOpr(StrUtil.toString(requestWrapper.getAttribute("FaLogOpr")));
+        logApi.setCrud((LogCrudEnum) requestWrapper.getAttribute("FaLogCrud"));
+
+        // request basic information
+        logApi.setUrl(requestWrapper.getRequestURI());
+        logApi.setMethod(requestWrapper.getMethod());
+        logApi.setAgent(requestWrapper.getHeader("User-Agent"));
+
+        // 解析agent字符串
+        UserAgent ua = UserAgentUtil.parse(logApi.getAgent());
+        logApi.setOs(ua.getOs().toString());
+        logApi.setBrowser(ua.getBrowser().toString());
+        logApi.setVersion(ua.getVersion());
+        logApi.setMobile(ua.isMobile() ? true : false);
+
+        // 自定义Header信息
+        logApi.setFaFrom(requestWrapper.getHeader(CommonConstants.FA_FROM));
+        try {
+            if (StrUtil.isNotEmpty(requestWrapper.getHeader(CommonConstants.FA_VERSION_CODE))) {
+                logApi.setVersionCode(Long.parseLong(requestWrapper.getHeader(CommonConstants.FA_VERSION_CODE)));
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        logApi.setVersionName(requestWrapper.getHeader(CommonConstants.FA_VERSION_NAME));
+
+        logApi.setCrtHost(IpUtils.getRequestIp(requestWrapper));
+        logApi.setRequest(requestWrapper.getBody());
+        logApi.setReqSize(logApi.getRequest().length());
+
+        logApi.setRetStatus(responseWrapper.getStatus());
+
+        if ("1".equals(logNoRet) && !"200".equals(logApi.getRetStatus())) {
+            logApi.setResponse("");
+        } else {
+            logApi.setResponse(responseData);
+        }
+
+        logApi.setRetSize(responseData.length());
+
+        logApi.setDuration(System.currentTimeMillis() - startTime);
+
+        // 获取IP地址
+        IpAddr ipAddr = IpUtils.getIpAddrByApi(logApi.getCrtHost());
+        if (ipAddr != null) {
+            logApi.setPro(ipAddr.getPro());
+            logApi.setCity(ipAddr.getCity());
+            logApi.setAddr(ipAddr.getAddr());
+        }
+
+        logApiBiz.save(logApi);
 
         BaseContextHandler.remove(); // 销毁上下文中记录的登录用户信息
     }
