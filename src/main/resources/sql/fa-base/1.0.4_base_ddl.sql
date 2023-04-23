37d38a1f151c2b7dc3f14c59ebaadbc1c2c22f26
xupengfei <faberxu@gmail.com>
2023-04-23 17:58:22 +0800

    feat: 修复crt_time字段更新自动更新

--- src/main/resources/sql/fa-base/1.0.4_base_ddl.sql
+++ src/main/resources/sql/fa-base/1.0.4_base_ddl.sql
@@ -3,7 +3,6 @@
 -- @@info: 修复crt_time字段更新自动更新
 -- ------------------------- info -------------------------
 
-ALTER TABLE `base_area` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
 ALTER TABLE `base_config` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
 ALTER TABLE `base_config_scene` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
 ALTER TABLE `base_config_sys` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
@@ -13,7 +12,6 @@ ALTER TABLE `base_entity_log` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAUL
 ALTER TABLE `base_file_biz` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
 ALTER TABLE `base_file_save` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
 ALTER TABLE `base_job` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
-ALTER TABLE `base_job_log` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
 ALTER TABLE `base_log_api` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
 ALTER TABLE `base_log_login` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
 ALTER TABLE `base_msg` MODIFY COLUMN `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
