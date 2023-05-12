a4275542da1aa795b7e7386a60fd7a38a25ae9ec
xupengfei <faberxu@gmail.com>
2023-05-12 16:16:07 +0800

    feat: hzx-小程序配置

--- src/main/resources/sql/fa-base/1.0.5_base_ddl.sql
+++ src/main/resources/sql/fa-base/1.0.5_base_ddl.sql
@@ -3,5 +3,5 @@
 -- @@info: base_file_biz字段调整：biz_id非必填、type必填
 -- ------------------------- info -------------------------
 
-ALTER TABLE `hzx_iot_dev`.`base_file_biz` MODIFY COLUMN `biz_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '业务ID' AFTER `main_biz_id`;
-ALTER TABLE `hzx_iot_dev`.`base_file_biz` MODIFY COLUMN `type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '业务类型' AFTER `biz_id`;
+ALTER TABLE `base_file_biz` MODIFY COLUMN `biz_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '业务ID' AFTER `main_biz_id`;
+ALTER TABLE `base_file_biz` MODIFY COLUMN `type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '业务类型' AFTER `biz_id`;
