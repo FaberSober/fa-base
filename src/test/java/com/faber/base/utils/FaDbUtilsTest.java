f09645eead67db3c732a42096c79a3815db9ba20
xupengfei <faberxu@gmail.com>
2023-05-31 11:39:51 +0800

    feat: base-分页方式循环获取数据

--- src/test/java/com/faber/base/utils/FaDbUtilsTest.java
+++ src/test/java/com/faber/base/utils/FaDbUtilsTest.java
@@ -1,9 +1,14 @@
 package com.faber.base.utils;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
 import com.faber.FaTestApp;
 import com.faber.api.base.rbac.biz.RbacMenuBiz;
 import com.faber.api.base.rbac.entity.RbacMenu;
 import com.faber.core.utils.FaDbUtils;
+import com.faber.core.vo.query.QueryParams;
+import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -41,6 +46,36 @@ public class FaDbUtilsTest {
                 },
                 10
         );
+    }
+
+    @Test
+    public void testLoopPageToExcel() {
+        String fileName = "/Users/xupengfei/tmp/testLoopPage.xlsx";
+
+        // 这里 需要指定写用哪个class去写
+        try (ExcelWriter excelWriter = EasyExcel.write(fileName, RbacMenu.class).build()) {
+            // 这里注意 如果同一个sheet只要创建一次
+            WriteSheet writeSheet = EasyExcel.writerSheet().build();
+
+            // 根据数据库分页去调用写入
+            FaDbUtils.loopPage(
+                    rbacMenuBiz.lambdaQuery(),
+                    pageInfo -> {
+                        log.debug("page: {}, isHasNextPage: {}, size: {}, pageSize: {}, total: {}", pageInfo.getPageNum(), pageInfo.isHasNextPage(), pageInfo.getSize(), pageInfo.getPageSize(), pageInfo.getTotal());
+                        List<RbacMenu> list = pageInfo.getList();
+                        excelWriter.write(list, writeSheet);
+                    },
+                    10
+            );
+        }
+    }
+
+    @Test
+    public void testBaseBizListPage() {
+        List<RbacMenu> list = rbacMenuBiz.list(new QueryParams());
+        for (RbacMenu menu : list) {
+            log.debug(menu.toString());
+        }
     }
 
 }
