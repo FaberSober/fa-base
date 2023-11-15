1b3fe64b7e47dee9fab8611935e9ddc12d7a5e3a
xupengfei <faberxu@gmail.com>
2023-11-15 15:40:33 +0800

    beam-智能排单-梁片生产周期管理-钢筋加工

--- src/main/java/com/faber/api/base/admin/biz/FileBizBiz.java
+++ src/main/java/com/faber/api/base/admin/biz/FileBizBiz.java
@@ -1,5 +1,6 @@
 package com.faber.api.base.admin.biz;
 
+import cn.hutool.core.util.StrUtil;
 import com.faber.api.base.admin.entity.FileSave;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,8 @@ import com.faber.api.base.admin.mapper.FileBizMapper;
 import com.faber.core.web.biz.BaseBiz;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * BASE-通用业务附件表
@@ -31,6 +34,42 @@ public class FileBizBiz extends BaseBiz<FileBizMapper,FileBiz> {
         entity.setExt(fileSave.getExt());
 
         super.saveBefore(entity);
+    }
+
+    public FileBiz saveFile(String mainBizId, String bizId, String type, String fileId) {
+        FileBiz fileBiz = new FileBiz();
+        fileBiz.setMainBizId(mainBizId);
+        fileBiz.setBizId(bizId);
+        fileBiz.setType(type);
+        fileBiz.setFileId(fileId);
+        this.save(fileBiz);
+        return fileBiz;
+    }
+
+    /**
+     * 批量保存业务数据
+     * @param mainBizId 主业务ID，如：订单ID
+     * @param bizId 业务ID，如：订单明细ID，可以为空
+     * @param type 业务类型，如：订单附件 ORDER_FILES
+     * @param fileIds 附件ID列表
+     * @return
+     */
+    public List<FileBiz> saveBatch(Object mainBizId, Object bizId, String type, List<String> fileIds) {
+        List<FileBiz> list = new ArrayList<>();
+        for (String fileId : fileIds) {
+            FileBiz fileBiz = this.saveFile(StrUtil.toString(mainBizId), StrUtil.toString(bizId), type, fileId);
+            list.add(fileBiz);
+        }
+        return list;
+    }
+
+    public List<FileBiz> getFiles(Object mainBizId, Object bizId, String type) {
+        return this.lambdaQuery()
+               .eq(FileBiz::getMainBizId, mainBizId)
+               .eq(FileBiz::getBizId, bizId)
+               .eq(FileBiz::getType, type)
+                .orderByAsc(FileBiz::getId)
+               .list();
     }
 
 }
