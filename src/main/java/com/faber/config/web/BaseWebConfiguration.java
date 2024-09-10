374cfa9ffe874ca555dd0d7ceedbd4ac35f703aa
xu.pengfei <1508075252@qq.com>
2024-09-10 16:53:54 +0800

    ai-debug sse

--- src/main/java/com/faber/config/web/BaseWebConfiguration.java
+++ src/main/java/com/faber/config/web/BaseWebConfiguration.java
@@ -1,5 +1,8 @@
 package com.faber.config.web;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.faber.core.config.exception.GlobalExceptionHandler;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,12 +10,16 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -68,6 +75,50 @@ public abstract class BaseWebConfiguration extends WebMvcConfigurationSupport {
         // 设置格式化内容
         converter.setObjectMapper(objectMapper);
         converters.add(0, converter);
+
+//        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+//        fastJsonConfig.setCharset(StandardCharsets.UTF_8);
+//        fastJsonConfig.setDateFormat(dateFormatPattern);
+//        fastJsonConfig.setSerializerFeatures(
+//                SerializerFeature.WriteClassName,
+//                SerializerFeature.WriteMapNullValue,
+//                SerializerFeature.PrettyFormat,
+//                SerializerFeature.WriteNullListAsEmpty,
+//                SerializerFeature.WriteNullStringAsEmpty
+//        );
+//
+//        List<MediaType> supportedMediaTypes = new ArrayList<>();
+//        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+//        supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+//        fastConverter.setSupportedMediaTypes(supportedMediaTypes);
+//
+//        converters.add(fastConverter);
+//
+//        this.addDefaultHttpMessageConverters(converters);
+
+//        List<MediaType> supportedMediaTypes = new ArrayList<>();
+//        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+//        supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+//        supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+//        supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+//        supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+//        supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+//        supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+//        supportedMediaTypes.add(MediaType.APPLICATION_XML);
+//        supportedMediaTypes.add(MediaType.IMAGE_GIF);
+//        supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+//        supportedMediaTypes.add(MediaType.IMAGE_PNG);
+//        supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+//        supportedMediaTypes.add(MediaType.TEXT_HTML);
+//        supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+//        supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+//        supportedMediaTypes.add(MediaType.TEXT_XML);
+//        converter.setSupportedMediaTypes(supportedMediaTypes);
+
+//        SSEEventSourceHttpMessageConverter sseEventSourceHttpMessageConverter = new SSEEventSourceHttpMessageConverter();
+//        converters.add(sseEventSourceHttpMessageConverter);
+
         super.extendMessageConverters(converters);
     }
 
