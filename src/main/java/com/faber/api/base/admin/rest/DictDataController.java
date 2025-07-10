d76551564ba608c4848d4edac2b1b74d8605467a
xu.pengfei <1508075252@qq.com>
2025-07-10 16:57:49 +0800

    base-dict-add value column

--- src/main/java/com/faber/api/base/admin/rest/DictDataController.java
+++ src/main/java/com/faber/api/base/admin/rest/DictDataController.java
@@ -3,9 +3,11 @@ package com.faber.api.base.admin.rest;
 import com.faber.api.base.admin.biz.DictDataBiz;
 import com.faber.api.base.admin.entity.DictData;
 import com.faber.core.annotation.FaLogBiz;
+import com.faber.core.annotation.FaLogOpr;
+import com.faber.core.enums.LogCrudEnum;
+import com.faber.core.vo.msg.Ret;
 import com.faber.core.web.rest.BaseTreeController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * BASE-字典值
@@ -18,5 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/base/admin/dictData")
 public class DictDataController extends BaseTreeController<DictDataBiz, DictData, Integer> {
+
+    @FaLogOpr(value = "切换默认", crud = LogCrudEnum.R)
+    @RequestMapping(value = "/toggleDefaultById/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Ret<Boolean> toggleDefaultById(@PathVariable Integer id) {
+        baseBiz.toggleDefaultById(id);
+        return ok();
+    }
 
 }
