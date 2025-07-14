392668b4676650077616f67b41e19ee2c7ee427d
ren.jinyi <894212241@qq.com>
2025-07-14 15:57:27 +0800

    库存不足预警相关需要开发

--- src/main/java/com/faber/api/base/admin/mapper/AlertMapper.java
+++ src/main/java/com/faber/api/base/admin/mapper/AlertMapper.java
@@ -1,7 +1,10 @@
 package com.faber.api.base.admin.mapper;
 
+import com.faber.api.base.admin.vo.ret.MaterialVo;
 import com.faber.core.config.mybatis.base.FaBaseMapper;
 import com.faber.api.base.admin.entity.Alert;
+
+import java.util.List;
 
 /**
  * BASE-告警信息
@@ -11,5 +14,12 @@ import com.faber.api.base.admin.entity.Alert;
  * @date 2023-12-16 11:40:20
  */
 public interface AlertMapper extends FaBaseMapper<Alert> {
+
+
+    /**
+     * 库存不足信息
+     * @return
+     */
+    List<MaterialVo> checkInventoryShortageAlerts();
 	
 }
