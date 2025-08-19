89132ed97a8b2675d75658fb4f1b4e26868d0d95
xu.pengfei <1508075252@qq.com>
2025-08-19 19:54:26 +0800

    base-file

--- src/main/java/com/faber/api/base/admin/biz/FileSaveBiz.java
+++ src/main/java/com/faber/api/base/admin/biz/FileSaveBiz.java
@@ -182,6 +182,9 @@ public class FileSaveBiz extends BaseBiz<FileSaveMapper, FileSave> implements St
     }
 
     public File getFileObj(FileSave fileSave) {
+        if (fileSave == null) {
+            throw new BuzzException("File Not Found");
+        }
         // 本地存储
         if (fileSave.getPlatform().startsWith("local-")) {
             LocalPlusFileStorage storage = ((LocalPlusFileStorage) fileStorageService.getFileStorage("local-plus-1"));
@@ -245,6 +248,9 @@ public class FileSaveBiz extends BaseBiz<FileSaveMapper, FileSave> implements St
      */
     public void getFilePreview(String fileId) throws IOException {
         FileSave fileSave = getById(fileId);
+        if (fileSave == null) {
+            return;
+        }
 
         // 本地存储
         if (fileSave.getPlatform().startsWith("local-")) {
