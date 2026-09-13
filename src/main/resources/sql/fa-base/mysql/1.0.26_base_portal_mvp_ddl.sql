0cf0b17e6bd531ecceb2c8474dd26776470ee228
xu.pengfei <1508075252@qq.com>
2026-09-13 20:38:59 +0800

    sync sql

--- src/main/resources/sql/fa-base/mysql/1.0.26_base_portal_mvp_ddl.sql
+++ src/main/resources/sql/fa-base/mysql/1.0.26_base_portal_mvp_ddl.sql
@@ -11,7 +11,7 @@ SET @ddl = IF(
    WHERE TABLE_SCHEMA = DATABASE()
      AND TABLE_NAME = 'base_user'
      AND COLUMN_NAME = 'admin_enabled') = 0,
-  'ALTER TABLE `base_user` ADD COLUMN `admin_enabled` tinyint(1) NOT NULL DEFAULT `0` COMMENT `是否允许访问后台管理端` AFTER `status`',
+  'ALTER TABLE `base_user` ADD COLUMN `admin_enabled` tinyint(1) NOT NULL DEFAULT 0 COMMENT "是否允许访问后台管理端" AFTER `status`',
   'SELECT 1'
 );
 PREPARE stmt FROM @ddl;
